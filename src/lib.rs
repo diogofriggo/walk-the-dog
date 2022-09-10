807868669432d44385988fa147c379d32de56548
Friggo <Diogo.Friggo@dnv.com>
2022-09-10 20:08:19 -0300

    delete idle

--- src/lib.rs
+++ src/lib.rs
@@ -61,34 +61,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let image = web_sys::HtmlImageElement::new().unwrap();
-
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = success_tx.send(Ok(()));
-            }
-        });
-
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-
-        let _ = success_rx.await.unwrap();
-        let _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        let side = 600.0;
-        let x = side / 2.0;
-        let y = 0.0;
-
         let json = fetch_json("rhb.json")
             .await
             .expect("Could not fetch rhb.json");
