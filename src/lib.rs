a3ff51e86bc62e0dd0f8a8f86ae9815a354dd2f7
Friggo <Diogo.Friggo@dnv.com>
2022-09-10 20:06:49 -0300

    delete sierpinski

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
+use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Mutex;
 
 use rand::prelude::*;
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 // use web_sys::console;
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
 
 // // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // // allocator.
@@ -68,10 +88,70 @@ pub fn main_js() -> Result<(), JsValue> {
         let side = 600.0;
         let x = side / 2.0;
         let y = 0.0;
+
         sierpinski(&context, x, y, side, "rgb(0, 255, 0)", 8);
+
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+
+        // COPY PASTE BEGINS
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                let _ = success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                let _ = error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("rhb.png");
+
+        let _ = success_rx.await.unwrap();
+
+        // COPY PASTE ENDS
+
+        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
+        let _ = context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                sprite.frame.x.into(),
+                sprite.frame.y.into(),
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+                200.0,
+                300.0,
+                sprite.frame.w.into(),
+                sprite.frame.h.into(),
+            );
     });
 
     Ok(())
+}
+
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    // window.fetch_with_str(json_path) returns a JS Promise
+    // which we then convert to a Rust Future so that we can await on it on the next line
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    // JS's dynamic typing into Rust's static typing
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
 
 fn sierpinski(
