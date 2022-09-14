3395c1cd5681f42aa2fc17a675c22bbd2e98b6b1
Friggo <Diogo.Friggo@dnv.com>
2022-09-13 21:39:36 -0300

    beginnings of a game loop

--- src/browser.rs
+++ src/browser.rs
@@ -1,8 +1,16 @@
+use std::borrow::Borrow;
+
 use anyhow::{anyhow, Result};
 use futures::Future;
-use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen::{
+    closure::{WasmClosure, WasmClosureFnOnce},
+    prelude::Closure,
+    JsCast, JsValue,
+};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, Response, Window};
+use web_sys::{
+    CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlImageElement, Response, Window,
+};
 
 macro_rules! log {
     ( $( $t:tt )* ) => {
@@ -67,4 +75,38 @@ pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
     )
     .await
     .map_err(|err| anyhow!("error fetching JSON {:#?}", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Could not create HtmlImageElement: {:#?}", err))
+}
+
+pub fn closure_once<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
+}
+
+pub type LoopClosure = Closure<dyn FnMut(f64)>;
+
+pub fn request_animation_frame(callback: &LoopClosure) -> Result<i32> {
+    window()?
+        .request_animation_frame(callback.as_ref().unchecked_ref())
+        .map_err(|err| anyhow!("Cannot request animation frame {:#?}", err))
+}
+
+pub fn create_raf_closure(f: impl FnMut(f64) + 'static) -> LoopClosure {
+    closure_wrap(Box::new(f))
+}
+
+pub fn closure_wrap<T: WasmClosure + ?Sized>(data: Box<T>) -> Closure<T> {
+    // let f: Rc<RefCell<Option<LoopClosure>>> = Rc::new(RefCell::new(None));
+    // let g = f.clone();
+    // let animate = Some(create_raf_closure(move |perf: f64| {
+    //     request_animation_frame(f.borrow().as_ref().unwrap());
+    // }));
+    // *g.borrow_mut() = animate;
+    // request_animation_frame(g.borrow().as_ref().unwrap());
+    Closure::wrap(data)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 mod browser;
 
+mod engine;
+
 use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Mutex;
@@ -48,31 +50,10 @@ pub fn main_js() -> Result<(), JsValue> {
             .into_serde()
             .expect("Could not convert rhb.json into a Sheet structure");
 
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let image = web_sys::HtmlImageElement::new().unwrap();
+        let image = engine::load_image("rhb.png")
+            .await
+            .expect("Could not load rhb.png");
 
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = success_tx.send(Ok(()));
-            }
-        });
-
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                let _ = error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("rhb.png");
-
-        let _ = success_rx.await.unwrap();
-
-        // Closure::once(move || { is able to convert to Close<dyn FnMut()> but Closure::wrap( isn't
-        // unlike Closure::once, Closure::wrap requires a Box, why?
         let mut frame = -1;
         let interval_callback = Closure::wrap(Box::new(move || {
             context.clear_rect(0.0, 0.0, 600.0, 600.0);
